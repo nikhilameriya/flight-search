9c0013f3b85512e2833739e0bbdcfb03f73cecb0
Nikhil Ameria <nikhil.ameria@agreeya.net>
2020-06-01 23:01:13 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Homepage from './Homepage';
+import SearchResultContainer from '../containers/SearchResultContainer';
+import Header from './Header';
+
+const App = () => (
+    <div>
+        <Header />
+        <div className="container main">
+            <Switch>
+                <Route path="/homepage" component={Homepage} />
+                <Route path="/search" component={SearchResultContainer} />
+                <Redirect from="/" to="/homepage" />
+            </Switch>
+        </div>
+    </div>
+)
+
+export default App;
---
+++ src/configuration.js
@@ -0,0 +1,5 @@
+const configuration = {};
+
+configuration.apiUrl = 'http://localhost:3000/api';
+
+export default configuration;
